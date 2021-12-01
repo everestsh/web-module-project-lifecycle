8280857bd4058b60b6bf16c237bcfb9ba1583f7e
sooof <sooof@me.com>
2021-11-30 23:59:46 -0800

    User.js work

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import User from './components/User';
 import FollowerList from './components/FollowerList';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      User: {},
+      Followers:[
+      ]
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/sooof')
+      .then(resp=> {
+        console.log(resp)
+          this.setState({
+              ...this.state,
+              User: resp.data,
+          });
+      })
+      .catch(err=> {
+          console.log(err);
+      })
+  }
   render() {
     return(
     <div>
@@ -26,7 +50,7 @@ class App extends React.Component {
         </nav>
         <div className="App">
           {/* <h1>Github Card</h1> */}
-          <User />
+          <User user={this.state.User}/>
           <FollowerList />
         </div>
       
